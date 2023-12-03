98b0b33167e019ad68fd75811cfc6e65ee0eb855
Superairjake <superairjake@gmail.com>
2023-12-02 19:19:02 -0700

    5

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Let's figure out your grade");
+        Console.WriteLine("What was your final percent? ");
+        string valueFromUser = Console.ReadLine();
+        int gradepercent = int.Parse(valueFromUser);
+
+        
+        {
+        if (gradepercent >= 90)
+        {
+            
+        }
+        else if (gradepercent >= 80);
+        {
+            
+        }
+        else if (gradepercent >= 70);
+        {
+           
+        }
+        else if (gradepercent >= 60);
+        {
+           
+        }
+        else
+        {
+           
+        }
+        }
+
+
     }
+    
 }
